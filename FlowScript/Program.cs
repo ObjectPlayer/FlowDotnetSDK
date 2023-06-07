3766e41e4c63501b2cfea5238c6594fdc7de9f00
Sham Gir <ObjectPlayer@gmail.com>
2023-06-07 15:08:42 +0500

    All commented code removed

--- FlowScript/Program.cs
+++ FlowScript/Program.cs
@@ -8,45 +8,45 @@ namespace FlowSdkTesting
         {
             FlowScriptTesting flowScriptTesting = new FlowScriptTesting();
 
-            // //Testing Boolean
-            // await flowScriptTesting.testBooleanType();
+            //Testing Boolean
+            await flowScriptTesting.testBooleanType();
 
-            // //Testing Numbers
-            // await flowScriptTesting.testIntType();
-            // await flowScriptTesting.testUIntType();
-            // await flowScriptTesting.testInt8Type();
-            // await flowScriptTesting.testUInt8Type();
-            // await flowScriptTesting.testInt16Type();
-            // await flowScriptTesting.testUInt16Type();
-            // await flowScriptTesting.testInt32Type();
-            // await flowScriptTesting.testUInt32Type();
-            // await flowScriptTesting.testInt64Type();
-            // await flowScriptTesting.testUInt64Type();
-            // await flowScriptTesting.testInt128Type();
-            // await flowScriptTesting.testUInt128Type();
-            // await flowScriptTesting.testInt256Type();
-            // await flowScriptTesting.testUInt256Type();
-            // await flowScriptTesting.testWord8Type();
-            // await flowScriptTesting.testWord16Type();
-            // await flowScriptTesting.testWord32Type();
-            // await flowScriptTesting.testWord64Type();
-            // await flowScriptTesting.testFix64Type();
-            // await flowScriptTesting.testUFix64Type();
+            //Testing Numbers
+            await flowScriptTesting.testIntType();
+            await flowScriptTesting.testUIntType();
+            await flowScriptTesting.testInt8Type();
+            await flowScriptTesting.testUInt8Type();
+            await flowScriptTesting.testInt16Type();
+            await flowScriptTesting.testUInt16Type();
+            await flowScriptTesting.testInt32Type();
+            await flowScriptTesting.testUInt32Type();
+            await flowScriptTesting.testInt64Type();
+            await flowScriptTesting.testUInt64Type();
+            await flowScriptTesting.testInt128Type();
+            await flowScriptTesting.testUInt128Type();
+            await flowScriptTesting.testInt256Type();
+            await flowScriptTesting.testUInt256Type();
+            await flowScriptTesting.testWord8Type();
+            await flowScriptTesting.testWord16Type();
+            await flowScriptTesting.testWord32Type();
+            await flowScriptTesting.testWord64Type();
+            await flowScriptTesting.testFix64Type();
+            await flowScriptTesting.testUFix64Type();
 
-            // //Testing Address
-            // await flowScriptTesting.testAddressType();
+            //Testing Address
+            await flowScriptTesting.testAddressType();
 
-            // //Testing String
-            // await flowScriptTesting.testStringType();
+            //Testing String
+            await flowScriptTesting.testStringType();
 
-            // //Testing Array
-            // await flowScriptTesting.testArrayType();
+            //Testing Array
+            await flowScriptTesting.testArrayType();
 
-            // //Testing Dictionary
-            // await flowScriptTesting.testDictionaryType();
+            //Testing Dictionary
+            await flowScriptTesting.testDictionaryType();
 
-            // //Testing Optional
-            // await flowScriptTesting.testOptionalType();
+            //Testing Optional
+            await flowScriptTesting.testOptionalType();
 
             // Testing Composite-Type
             await flowScriptTesting.testCompositeType();
